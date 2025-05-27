495b804db2980f2e23abd14016222c84e4b84a10
ngocdat784 <ngocdattran315@gmail.com>
2025-05-27 11:01:44 +0700

    Lab02

--- Lab02/api.py
+++ Lab02/api.py
@@ -3,13 +3,16 @@ from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.playfair import PlayFairCipher
-from cipher.transposition import TranspositionCipher  # Thêm dòng này
 
 app = Flask(__name__)
 
-# CAESAR CIPHER ALGORITHM
+# Khởi tạo các đối tượng mã hóa, đảm bảo dùng chung suốt quá trình ứng dụng chạy
 caesar_cipher = CaesarCipher()
+vigenere_cipher = VigenereCipher()
+railfence_cipher = RailFenceCipher()
+playfair_cipher = PlayFairCipher()
 
+# -------------------------------- CAESAR CIPHER --------------------------------
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
     data = request.json
@@ -26,10 +29,7 @@ def caesar_decrypt():
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
-
-# VIGENERE CIPHER ALGORITHM
-vigenere_cipher = VigenereCipher()
-
+# ------------------------------- VIGENERE CIPHER -------------------------------
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
     data = request.json
@@ -46,10 +46,7 @@ def vigenere_decrypt():
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
-
-# RAILFENCE CIPHER ALGORITHM
-railfence_cipher = RailFenceCipher()
-
+# ------------------------------ RAIL FENCE CIPHER ------------------------------
 @app.route('/api/railfence/encrypt', methods=['POST'])
 def railfence_encrypt():
     data = request.json
@@ -66,10 +63,7 @@ def railfence_decrypt():
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
-
-# PLAYFAIR CIPHER ALGORITHM
-playfair_cipher = PlayFairCipher()
-
+# ------------------------------- PLAYFAIR CIPHER -------------------------------
 @app.route('/api/playfair/creatematrix', methods=['POST'])
 def playfair_creatematrix():
     data = request.json
@@ -95,27 +89,6 @@ def playfair_decrypt():
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
     return jsonify({'decrypted_text': decrypted_text})
 
-
-# TRANSPOSITION CIPHER ALGORITHM
-transposition_cipher = TranspositionCipher()
-
-@app.route('/api/transposition/encrypt', methods=['POST'])
-def transposition_encrypt():
-    data = request.get_json()
-    plain_text = data.get('plain_text')
-    key = int(data.get('key'))
-    encrypted_text = transposition_cipher.encrypt(plain_text, key)
-    return jsonify({'encrypted_text': encrypted_text})
-
-@app.route('/api/transposition/decrypt', methods=['POST'])
-def transposition_decrypt():
-    data = request.get_json()
-    cipher_text = data.get('cipher_text')
-    key = int(data.get('key'))
-    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
-    return jsonify({'decrypted_text': decrypted_text})
-
-
-# main function
-if __name__ == "__main__":
+# ----------------------------- CHẠY FLASK SERVER -------------------------------
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
